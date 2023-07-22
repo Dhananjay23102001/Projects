77a68b4586eea4125978b03126bf49f4f2e4b835
Dhananjay23102001 <140022284+Dhananjay23102001@users.noreply.github.com>
2023-07-22 17:29:34 +0530

    Initial commit

--- Student_Enquiry_Management_System/src/main/java/in/skilltech/enquiry_management/ServletInitializer.java
+++ Student_Enquiry_Management_System/src/main/java/in/skilltech/enquiry_management/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FrontOfficeTeamPortalProjectApplication.class);
+		return application.sources(SkilltechEnquiryManagementApplication.class);
 	}
 
 }
